d7a278ebedaacca5fc1cc84f4b09bd4aec9da2f1
Nicolas Van der Werf <nicvanderwerf@gmail.com>
2022-12-18 16:57:01 -0700

    final

--- webFunctions.js
+++ webFunctions.js
@@ -1,7 +1,31 @@
-counter = 0;
 
-function increment() {
-    console.log("Clicked: " + counter + " times");
-    counter++;
-    document.getElementById("id01").innerHTML = counter;
-}
+var totalMovement = 0;
+
+
+document.addEventListener('wheel',function(e) {
+    if(Math.abs(e.deltaY) < 5){return;}
+    totalMovement += e.deltaY/10;
+    var min = false;
+    if(totalMovement > 140){totalMovement = 140;min=true;}
+    if(totalMovement < 0){totalMovement = 0;}
+    const mainDiv = document.getElementById("mainName")
+    if(min){
+        mainDiv.style.transition = '300ms linear';
+        mainDiv.style.width = '100%';
+        mainDiv.style.height = '10vh';
+        mainDiv.style.lineHeight = '10vh';
+        mainDiv.style.textAlign = 'left';
+
+        document.getElementById("nameSpan").style.fontSize = 40-(totalMovement/4) +'vmax';
+
+        
+    }else{
+        mainDiv.style.textAlign = 'center';
+        mainDiv.style.width = 90-(totalMovement/2) +'vw';
+        mainDiv.style.transition = '0ms linear';
+        mainDiv.style.height = 90-(totalMovement/2) +'vh';
+        mainDiv.style.lineHeight = 90-(totalMovement/2) +'vh';
+        document.getElementById("nameSpan").style.fontSize = 40-(totalMovement/4) +'vmax';
+        // div.style.height = window.scrollY +'px';
+    }
+});
