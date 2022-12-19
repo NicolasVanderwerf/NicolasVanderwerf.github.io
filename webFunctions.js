2eca9ecf4b433f09ecac313f70ddf5266ac9cdf5
Nicolas Van der Werf <nicvanderwerf@gmail.com>
2022-12-18 23:09:04 -0700

    Final, final

--- webFunctions.js
+++ webFunctions.js
@@ -1,6 +1,20 @@
 
 var totalMovement = 0;
 
+const map1 = new Map();
+map1.set(0, "Try not. Do or do not, there is no try. - Yoda");
+map1.set(1, "Your eyes can desieve you; dont trust them. - Obi-Wan Kenobi");
+map1.set(2, "The ability to speak does not make you intelligent. - Qui-Gon Jin");
+map1.set(3, "I like firsts. Good or bad, they are always memorable - Ahsoka Tano");
+map1.set(4, "Sometimes we must let go of our pride and do what is requested of us. - Anakin Skywalker");
+map1.set(5, "Many of the truths that we cling to depend on our view point. - Obi-Wan Kenobi");
+map1.set(6, "Train yourself to let go of everything you fear to lose. - Yoda");
+map1.set(7, "No longer certain that one ever does win a war, I am. - Yoda");
+map1.set(8, "The Dark Side leads to many abilities some would consider to be unatural. - Shiv");
+function getQuote(){
+    document.getElementById("reflectionSpan").innerHTML = 'Quote:';
+    document.getElementById("reflectionP").innerText = map1.get(Math.floor(Math.random() * 8));
+}
 
 document.addEventListener('wheel',function(e) {
     if(Math.abs(e.deltaY) < 5){return;}
@@ -27,5 +41,10 @@ document.addEventListener('wheel',function(e) {
         mainDiv.style.lineHeight = 90-(totalMovement/2) +'vh';
         document.getElementById("nameSpan").style.fontSize = 40-(totalMovement/4) +'vmax';
         // div.style.height = window.scrollY +'px';
+    }
+    if(Math.abs(e.deltaX) > 100){
+        console.log(e.deltaX)
+        let beat = new Audio('10942 S Pleasant Hill Cir.m4a');
+        beat.play();
     }
 });
