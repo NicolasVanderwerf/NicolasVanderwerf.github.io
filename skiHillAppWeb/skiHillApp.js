6360a6c5855cf0eb434cb1f24e8f00c9081cf14f
nicolas1440 <104225806+Nicolas1440@users.noreply.github.com>
2023-05-23 10:19:52 -0600

    skiHillApp

---
+++ skiHillAppWeb/skiHillApp.js
@@ -0,0 +1,27 @@
+let pdfScale = 1; // default scale
+let canvas = document.querySelector("#pdf-canvas");
+let context = canvas.getContext('2d');
+
+pdfjsLib.getDocument('/skiHillMaps/ParkCitySkiHillMap.pdf').promise.then((pdfDoc_) => {
+    pdfDoc = pdfDoc_;
+    renderPage(1); // display first page
+});
+
+function renderPage(num) {
+    pdfDoc.getPage(num).then((page) => {
+        let viewport = page.getViewport({ scale: pdfScale });
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        let renderContext = {
+            canvasContext: context,
+            viewport: viewport,
+        };
+        page.render(renderContext);
+    });
+}
+
+canvas.addEventListener('click', (event) => {
+    let x = event.pageX - canvas.offsetLeft;
+    let y = event.pageY - canvas.offsetTop;
+    console.log(`PDF coordinates: (${x / pdfScale}, ${y / pdfScale})`);
+});
